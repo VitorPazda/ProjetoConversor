57b07e3b9a1a6dbea4797137855bb4ef54c5c3d0
Vitor Pazda <vitorpazda@gmail.com>
2026-08-19 15:50:38 -0300

    :memo: Removendo o construtor da classe User

--- ProjetoConversor.Server/Models/User.cs
+++ ProjetoConversor.Server/Models/User.cs
@@ -7,16 +7,8 @@ namespace ProjetoConversor.Models
     {
         [Key]
         public int IdUser { get; set; }
-        public string Name { get; set; }
-        public string AccountType { get; set; }
-        public string Password { get; set; }
-
-        public User(int idUser, string name, string accountType, string password)
-        {
-            IdUser = idUser;
-            Name = name;
-            AccountType = accountType;
-            Password = password;
-        }
+        public string Name { get; set; } = string.Empty;
+        public string AccountType { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
     }
 }
