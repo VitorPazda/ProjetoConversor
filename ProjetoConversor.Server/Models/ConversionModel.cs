31b7b93f446e8660357902b169e69600963d6719
Vitor Pazda <vitorpazda@gmail.com>
2026-08-19 14:54:46 -0300

    :memo: Att o construtor de Conversion

--- ProjetoConversor.Server/Models/ConversionModel.cs
+++ ProjetoConversor.Server/Models/ConversionModel.cs
@@ -12,5 +12,15 @@ namespace ProjetoConversor.Models
         public string FileName { get; set; }
         public DateTime Date { get; set; }
         public string Status { get; set; }
+
+        public ConversionModel(int idConversion, int userId, string bank, string fileName, DateTime date, string status)
+        {
+            IdConversion = idConversion;
+            UserId = userId;
+            Bank = bank;
+            FileName = fileName;
+            Date = date;
+            Status = status;
+        }
     }
 }
